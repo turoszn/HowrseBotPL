c17fcf35772bd30abf92ff426a6020641c06b3c1
Natalia Turosz <48881422+turoszn@users.noreply.github.com>
2020-04-16 21:50:27 +0200

    Update klik.py

--- klik.py
+++ klik.py
@@ -56,89 +56,101 @@ doKonika1 = browser.find_element_by_class_name("horsename").click()
 i = 0
 
 while i <= 10:  # jeśli napotkamy 10 oporzadzone konie to skrypt sie zatrzyma. mo
-    print("Oporządzanie konia...")
-    kolejny = browser.find_element_by_id("nav-next")  # przycisk "kolejny"
-    poprzedni = browser.find_element_by_id("nav-previous")  # przycisk "poprzedni"
+  try:  # sprawdzamy czy nie rodzi
+        browser.find_element_by_id("boutonVeterinaire")
+        rodzi = True
+    except NoSuchElementException:
+        rodzi = False
+    if rodzi:
+        browser.find_element_by_id("boutonVeterinaire").click()
+        time.sleep(randint(2, 5))
+        imie_zreba = browser.find_element_by_class_name("input")
+        imie_zreba.send_keys("Krezus Junior")
+        browser.find_element_by_xpath("//span[text()='Kontynuuj']").click()
+
     try:  # sprawdzamy czy koń oporzadzony
         browser.find_element_by_xpath("//a[contains(@class, 'panser') and contains(@class, 'action-disabled')]")
         oporzadzony = True
-        i = i+1
     except NoSuchElementException:
         oporzadzony = False
 
-        try:  # sprawdzamy czy koń to źreb
-            browser.find_element_by_id("boutonAllaiter")
-            zreb = True
+    try:  # sprawdzamy czy koń to źreb
+        browser.find_element_by_id("boutonAllaiter")
+        zreb = True
+    except NoSuchElementException:
+        zreb = False
+    if zreb:  # jeśli koń to źreb
+        if not oporzadzony:
+            butla = browser.find_element_by_id("boutonAllaiter").click()
+            time.sleep(randint(0, 5))
+            spanko = browser.find_element_by_id("boutonCoucher").click()
+            time.sleep(randint(0, 5))
+            czesanko = browser.find_element_by_id("boutonPanser").click()
+        else:
+            kolejny = browser.find_element_by_id("nav-next").click()
+            # kolejny = browser.find_element_by_id("nav-previous").click()
+    else:  # jeśli koń żre już siano
+        try:  # sprawdzamy czy koń jest w ośrodku
+            browser.find_element_by_xpath("//span[text()=' w ośrodku jeździeckim, aby położyć go spać już teraz!']")
+            pozaOsrodkiem = True
         except NoSuchElementException:
-            zreb = False
-        if zreb:  # jeśli koń to źreb
-            if not oporzadzony:
-                butla = browser.find_element_by_id("boutonAllaiter").click()
-                time.sleep(randint(0, 5))
-                spanko = browser.find_element_by_id("boutonCoucher").click()
-                time.sleep(randint(0, 5))
-                czesanko = browser.find_element_by_id("boutonPanser").click()
-            else:
-                kolejny = browser.find_element_by_id("nav-next").click()
-        else:  # jeśli koń żre już siano
-            try:  # sprawdzamy czy koń jest w ośrodku
-                browser.find_element_by_xpath("//span[text()=' w ośrodku jeździeckim, aby położyć go spać już teraz!']")
-                pozaOsrodkiem = True
+            pozaOsrodkiem = False
+        if pozaOsrodkiem:
+            time.sleep(randint(2, 5))
+            rejestracja = browser.find_element_by_xpath("//a[text()='Zarejestruj swojego konia']").click()
+            time.sleep(randint(3, 5))
+            dni60 = browser.find_element_by_xpath("//a[text()='60 dni']").click()
+            time.sleep(randint(5, 6))
+            eq1200 = browser.find_element_by_xpath("//strong[text()='1 200']").click()
+            time.sleep(randint(2, 5))
+        if not oporzadzony:
+            spanko = browser.find_element_by_id("boutonCoucher").click()
+            time.sleep(randint(0, 5))
+            czesanko = browser.find_element_by_id("boutonPanser").click()
+            time.sleep(randint(2, 5))
+            siano = browser.find_element_by_id("boutonNourrir").click()
+            time.sleep(randint(0, 5))
+            nakarm = browser.find_element_by_id("feed-button")
+            try:
+                browser.find_element_by_xpath("//span[text()='Uwaga: Twoja klacz ma niedowagę, musisz jej podać następującą ilość paszy: 20, aby wróciła do formy!'] | //span[text()='Uwaga: jeden z Twoich koni ma niedowagę, musisz mu podać następującą ilość paszy: 20, aby wrócił do formy!'] ")
+                uwagaChudy = True
             except NoSuchElementException:
-                pozaOsrodkiem = False
-            if pozaOsrodkiem:
-                time.sleep(randint(2, 5))
-                rejestracja = browser.find_element_by_xpath("//a[text()='Zarejestruj swojego konia']").click()
+                 uwagaChudy = False
+            try:
+                browser.find_element_by_xpath("//span[text()='Twoja klacz jest zbyt gruba. Nie karm jej dziś, aby mogła powrócić do normalnej wagi!'] | //span[text()='Twój koń jest zbyt gruby. Nie karm go dziś, aby mógł powrócić do normalnej wagi!']")
+                uwagaGruby = True
+            except NoSuchElementException:
+                uwagaGruby = False
+            if uwagaChudy:  # warunkowe karmienia gruby/chudy/normalny
+                pasza20 = browser.find_element_by_xpath("//span[text()='20']").click()
                 time.sleep(randint(0, 5))
-                dni60 = browser.find_element_by_xpath("//a[text()='60 dni']").click()
-                time.sleep(randint(5))
-                eq1200 = browser.find_element_by_xpath("//strong[text()='1 200']").click()
-                time.sleep(randint(2, 5))
-            if not oporzadzony:
-                spanko = browser.find_element_by_id("boutonCoucher").click()
+                nakarm.click()
+            elif uwagaGruby:
+                niekarm = browser.find_element_by_class_name("float-right")
                 time.sleep(randint(0, 5))
-                czesanko = browser.find_element_by_id("boutonPanser").click()
-                time.sleep(randint(2, 5))
-                siano = browser.find_element_by_id("boutonNourrir").click()
+                niekarm.click()
+            else:
+                iloscZarcia = browser.find_element_by_class_name("section-fourrage-target")
+                x = iloscZarcia.text
+                paszaX = browser.find_element_by_xpath("//span[contains(@class, 'slider-number') and contains(text(), %s)]" % x).click()
+                # paszaX = browser.find_element_by_xpath("//span[text()=%s]" % x).click()
                 time.sleep(randint(0, 5))
-                nakarm = browser.find_element_by_id("feed-button")
-                try:
-                    browser.find_element_by_xpath("//span[text()='Uwaga: Twoja klacz ma niedowagę, musisz jej podać następującą ilość paszy: 20, aby wróciła do formy!'] | //span[text()='Uwaga: jeden z Twoich koni ma niedowagę, musisz mu podać następującą ilość paszy: 20, aby wrócił do formy!'] ")
-                    uwagaChudy = True
-                except NoSuchElementException:
-                    uwagaChudy = False
-                try:
-                    browser.find_element_by_xpath("//span[text()='Twoja klacz jest zbyt gruba. Nie karm jej dziś, aby mogła powrócić do normalnej wagi!'] | //span[text()='Twój koń jest zbyt gruby. Nie karm go dziś, aby mógł powrócić do normalnej wagi!']")
-                    uwagaGruby = True
-                except NoSuchElementException:
-                    uwagaGruby = False
-                if uwagaChudy:  # warunkowe karmienia gruby/chudy/normalny
-                    pasza20 = browser.find_element_by_xpath("//span[text()='20']").click()
-                    time.sleep(randint(0, 5))
-                    nakarm.click()
-                elif uwagaGruby:
-                    niekarm = browser.find_element_by_class_name("float-right")
-                    time.sleep(randint(0, 5))
-                    niekarm.click()
-                else:
-                    iloscZarcia = browser.find_element_by_class_name("section-fourrage-target")
-                    x = iloscZarcia.text
-                    paszaX = browser.find_element_by_xpath("//span[text()=%s]" % x).click()
-                    time.sleep(randint(0, 5))
-                    nakarm.click()
+                nakarm.click()
 
-                    try:  # sprawdzamy czy koń może robić zlecenia
-                        browser.find_element_by_id("boutonMissionEquus")
-                        zlecenie = True
-                    except NoSuchElementException:
-                        zlecenie = False
+            try:  # sprawdzamy czy koń może robić zlecenia
+                zlecenie_klik= browser.find_element_by_xpath("//a[contains(@class, 'missionEquus') or contains(@class, 'missionForet')  or contains(@class, 'missionMontagne')]")
+                zlecenie1 = True
+            except NoSuchElementException:
+                zlecenie1 = False
 
-                    if zlecenie:
-                        klikZlecenie = browser.find_element_by_id("boutonMissionEquus").click()
-                        time.sleep(randint(0, 5))
+            if zlecenie1:
+                zlecenie_klik.click()
+                time.sleep(randint(0, 5))
             else:
                 kolejny = browser.find_element_by_id("nav-next").click()
+                # kolejny = browser.find_element_by_id("nav-previous").click()
     kolejny = browser.find_element_by_id("nav-next").click()
+    # kolejny = browser.find_element_by_id("nav-previous").click()
 
 
 print("Koniec Psot!")
